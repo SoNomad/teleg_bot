6ec788de1f8e34732ea903d4061dc3bc1cc8a1f6
albert.tasuev <n.tasuev@sreda.capital>
2025-03-08 22:44:48 +0300

    mend

--- bot-main.js
+++ bot-main.js
@@ -2,6 +2,7 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf("7422929739:AAEYb118A7MNpQ5w4CEJMx7mkbYb-ViIjGA");
 const adminId = "5075515168"; // ID админа, куда идут вопросы
+const userQuestions = new Map(); // Храним userId по сообщению
 
 // Стартовое сообщение
 const welcomeMessage = `без негативчика\n\n<i>Создано с помощью <a href="http://t.me/refatherbot?start=iagktybot">@ReFatherBot</a></i>`;
@@ -18,28 +19,24 @@ bot.on("text", async (ctx) => {
 
   await ctx.telegram.sendMessage(
     adminId,
-    `Вопрос от: ${userName} (ID: ${userId}):\n${ctx.message.text}`,
-    { reply_markup: { force_reply: true } } // Добавляем "ответить"
+    `Вопрос от: ${userName} (ID: ${userId}):\n${ctx.message.text}`
   );
+  userQuestions.set(sentMessage.message_id, userId); // Связываем сообщение и userId
 });
 
 // Обработчик ответов от админа
 bot.on("message", async (ctx) => {
   if (!ctx.message.reply_to_message) return; // Если не ответ — игнорируем
 
-  const replyToMessage = ctx.message.reply_to_message.text; // Оригинальный вопрос
-  const userIdMatch = replyToMessage.match(/\(ID: (\d+)\)/); // Извлекаем userId
+  const repliedMessageId = ctx.message.reply_to_message.message_id; // ID сообщения, на которое ответили
+  const userId = userQuestions.get(repliedMessageId); // Ищем userId в хранилище
 
-  if (!userIdMatch)
-    return ctx.reply(
-      "⚠️ Не могу определить, кому отправить ответ. Аккаунт без юзернейма"
-    );
+  if (!userId) return ctx.reply("⚠️ Не могу определить, кому отправить ответ.");
 
-  const userId = userIdMatch[1]; // Берём userId из скобок
   const replyText = ctx.message.text; // Ответ админа
 
   await ctx.telegram.sendMessage(userId, `${replyText}`);
-  ctx.reply("✅ Ответ отправлен пользователю.");
+  ctx.reply(`✅ Ответ отправлен пользователю ${userId}.`);
 });
 
 bot.launch();
